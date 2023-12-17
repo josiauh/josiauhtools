1e65a0bc98109ddbf72730fe46b8d20a6cd6afd7
Josiah <65928867+josaih-mo@users.noreply.github.com>
2023-12-17 09:50:24 +0900

    try excepts

--- src/josiauhtools_josiauh/__main__.py
+++ src/josiauhtools_josiauh/__main__.py
@@ -6,11 +6,14 @@ print("Josiauhtools")
 print(yachalk.chalk.red_bright("Unstabler ") + "0.0.3")
 parser = argparse.ArgumentParser()
 parser.add_argument("tool", help="The tool to use. If specified, the help will be dynamic.")
+try:
+    if (sys.argv[1] == "exvenvded"):
+        parser.add_argument("subtool", help="The subtool to use.")
+        if (sys.argv[2] == "convert"):
+            parser.add_argument("path", help="The path to work with. The default is the current directory.", default=".")
+except:
+    pass
 
-if (sys.argv[1] == "exvenvded"):
-    parser.add_argument("subtool", help="The subtool to use.")
-    if (sys.argv[2] == "convert"):
-        parser.add_argument("path", help="The path to work with. The default is the current directory.", default=".")
 
 args = parser.parse_args()
 
